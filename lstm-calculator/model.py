cebcce84375a2bce4809c0b92dd18d894b9f388c
Eddie Xie <oeddyo@gmail.com>
2024-02-05 07:30:59 -0800

    Separate dimensions

--- lstm-calculator/model.py
+++ lstm-calculator/model.py
@@ -1,13 +1,12 @@
-import numpy as np
 import torch
 from torch import nn
 
 
 class Encoder(nn.Module):
-    def __init__(self, voc_size, num_embeddings=100, num_layers=3):
+    def __init__(self, voc_size, num_embeddings=100, num_hidden=100, num_layers=3):
         super().__init__()
         self.emb = nn.Embedding(voc_size, num_embeddings)
-        self.lstm = nn.LSTM(num_embeddings, num_embeddings, num_layers=num_layers, batch_first=True)
+        self.lstm = nn.LSTM(num_embeddings, num_hidden, num_layers=num_layers, batch_first=True, dropout=0.5)
 
     def forward(self, x):
         x = self.emb(x)
@@ -16,11 +15,11 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, voc_size, num_embeddings=100, num_layers=3):
+    def __init__(self, voc_size, num_embeddings=100, num_hidden=100, num_layers=3):
         super().__init__()
         self.emb = nn.Embedding(voc_size, num_embeddings)
-        self.lstm = nn.LSTM(num_embeddings, num_embeddings, num_layers, batch_first=True)
-        self.linear = nn.Linear(num_embeddings, voc_size, )
+        self.lstm = nn.LSTM(num_embeddings, num_hidden, num_layers, batch_first=True, dropout=0.5)
+        self.linear = nn.Linear(num_hidden, voc_size, )
 
     def forward(self, x, h):
         x = self.emb(x)
@@ -32,8 +31,8 @@ class Decoder(nn.Module):
 class CalculatorModel(nn.Module):
     def __init__(self, voc_size, char_to_index, index_to_char):
         super().__init__()
-        self.encoder = Encoder(voc_size)
-        self.decoder = Decoder(voc_size)
+        self.encoder = Encoder(voc_size, num_embeddings=16, num_hidden=128)
+        self.decoder = Decoder(voc_size, num_embeddings=16,  num_hidden=128)
         self.char_to_index = char_to_index
         self.index_to_char = index_to_char
 
@@ -53,7 +52,8 @@ class CalculatorModel(nn.Module):
 
             prob = torch.softmax(output, dim=1)[-1]
 
-            next_char_index = torch.multinomial(prob, num_samples=1).item()
+            #next_char_index = torch.multinomial(prob, num_samples=1).item()
+            next_char_index = torch.argmax(prob).item()
             if self.index_to_char[next_char_index] == ' ':
                 break
             cur.append(next_char_index)
--- lstm-calculator/train_calculator.py
+++ lstm-calculator/train_calculator.py
@@ -24,23 +24,24 @@ def compute_accuracy(model, x_val, y_val, index_to_char, show_sample=False):
     model.eval()
     eq = 0
     tot = 0
-    for i in range(x_val.shape[0]):
-        computed = model.sample(x_val[i])
-        v = int("".join([index_to_char[idx] for idx in y_val[i].tolist()][1:]))
-        computed = computed.lstrip("0")
+    with torch.no_grad():
+        for i in range(x_val.shape[0]):
+            computed = model.sample(x_val[i])
+            v = int("".join([index_to_char[idx] for idx in y_val[i].tolist()][1:]))
+            computed = computed.lstrip("0")
 
-        if show_sample and i < 3:
-            print('sampled results: ', computed, v)
+            if show_sample and i < 3:
+                print('sampled results: ', computed, v)
 
-        try:
-            computed_int = int(computed)
-            if computed_int == v:
-                eq += 1
-        except ValueError:
-            pass
-        tot += 1
+            try:
+                computed_int = int(computed)
+                if computed_int == v:
+                    eq += 1
+            except ValueError:
+                pass
+            tot += 1
 
-    print('accuracy = ', eq * 1.0 / tot)
+        print('accuracy = ', eq * 1.0 / tot)
 
 
 def main():
@@ -54,9 +55,9 @@ def main():
     model = CalculatorModel(voc_size, char_to_index, index_to_char)
     optimizer = Adam(model.parameters(), lr=1e-3)
     criterion = CrossEntropyLoss()
-    training_loader = DataLoader(AdditionDataset(x_train, y_train), batch_size=16, drop_last=True)
+    training_loader = DataLoader(AdditionDataset(x_train, y_train), batch_size=128, drop_last=True)
 
-    for epoch in range(25):
+    for epoch in range(50):
         model.train()
         progress_bar = tqdm(training_loader, desc=f"Epoch {epoch + 1}")
 
